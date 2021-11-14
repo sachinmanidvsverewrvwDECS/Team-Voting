99ac15a83345b3d3b26c8918f241174d23bbc231
sachinmanidvsverewrvwDECS <90509273+sachinmanidvsverewrvwDECS@users.noreply.github.com>
2021-11-14 07:16:03 -0800

    Add files via upload

--- config.js
+++ config.js
@@ -1,9 +1,19 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
 
-};
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+    apiKey: "AIzaSyDahEopIkKu5bSwVacKPsBSoTFZWUAqLRI",
+    authDomain: "attendence-e999d.firebaseapp.com",
+    databaseURL: "https://attendence-e999d-default-rtdb.firebaseio.com",
+    projectId: "attendence-e999d",
+    storageBucket: "attendence-e999d.appspot.com",
+    messagingSenderId: "907451577399",
+    appId: "1:907451577399:web:84f02450290fe63458bbb2",
+    measurementId: "G-STB1705312"
+  };
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
